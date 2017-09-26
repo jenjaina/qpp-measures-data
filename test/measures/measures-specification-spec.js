c9b525cf8caefbcd94d82760a44bff53c76b3509
Biju Joseph <biju.joseph@semanticbits.com>
2017-09-26 10:51:46 -0400

    changes related to clinical cluster - incrementing version

---
+++ test/measures/measures-specification-spec.js
@@ -0,0 +1,36 @@
+const chai = require('chai');
+const assert = chai.assert;
+var request = require('sync-request');
+
+var measuresData = require('../../index.js');
+
+describe('measures specification', function () {
+
+  it('should have valid specification links', function (done) {
+    var measures = measuresData.getMeasuresData();
+    var specifications = measures
+      .map(m => ({measureId: m.measureId, measureSpecification: m.measureSpecification}))
+      .filter(s => !!s.measureSpecification);
+
+    var statuses = [];
+    specifications.forEach(s => {
+      Object.keys(s.measureSpecification).forEach(key => {
+        var url = s.measureSpecification[key];
+        var body = request('HEAD', url);
+        var valid = /4\d\d/.test(body.statusCode) === false;
+        if (!valid) {
+          statuses.push({measureId: s.measureId, submissionMethod: key, httpStatus: body.statusCode})
+        }
+
+      })
+    });
+
+    if (statuses.length > 0) {
+      console.table(statuses);
+    }
+    // assert.equal(0, statuses.length, "One or more measure specifications link is invalid");
+    done();
+  });
+
+
+});
