676f3016ee035e5039e1ec24affe2bdd6479932e
Biju Joseph <biju.joseph@semanticbits.com>
2017-09-26 23:09:49 -0400

    fixing lint errors

--- test/measures/measures-specification-spec.js
+++ test/measures/measures-specification-spec.js
@@ -4,9 +4,8 @@ var request = require('sync-request');
 
 var measuresData = require('../../index.js');
 
-describe('measures specification', function () {
-
-  it('should have valid specification links', function (done) {
+describe('measures specification', function() {
+  it('should have valid specification links', function(done) {
     this.timeout(300000); // 5 minutes timeout.
 
     var measures = measuresData.getMeasuresData();
@@ -18,24 +17,19 @@ describe('measures specification', function () {
     specifications.forEach(s => {
       Object.keys(s.measureSpecification).forEach(key => {
         var url = s.measureSpecification[key];
-        console.log("Checking: ", url);
+        console.log('Checking: ', url);
         var body = request('HEAD', url);
         var valid = /4\d\d/.test(body.statusCode) === false;
         if (!valid) {
-          statuses.push({measureId: s.measureId, submissionMethod: key, httpStatus: body.statusCode})
+          statuses.push({measureId: s.measureId, submissionMethod: key, httpStatus: body.statusCode});
         }
-
-      })
+      });
     });
 
     if (statuses.length > 0) {
       console.table(statuses);
     }
-
     assert.equal(0, statuses.length, 'One or more measure specifications link is invalid');
-
     done();
   });
-
-
 });
